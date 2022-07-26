f393edf7f56dc9cfc5045db048e336253304a925
rangashin <99878706+rangashin@users.noreply.github.com>
2022-07-26 22:21:26 +0800

    Add project files.

---
+++ LoginForm/MainWindow.xaml.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using MaterialDesignThemes.Wpf;
+
+namespace LoginForm
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        /*SqlConnection con = new SqlConnection(@"Data Source=localhost;Initial Catalog=pos;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+        CustomerOrderWindow sales = new CustomerOrderWindow();
+        private void btnLoginClick(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                string sql = @"SELECT * FROM tbluser WHERE username=@username AND password=@password";
+                SqlCommand cmd = new SqlCommand(sql, con);
+                cmd.CommandType = CommandType.Text;
+                cmd.Parameters.AddWithValue("@username", username.Text);
+                cmd.Parameters.AddWithValue("@password", password.Text);
+                con.Open();
+                //MessageBox.Show(username.Text, password.Text);
+
+                //cmd.ExecuteNonQuery();
+                int user_id = Convert.ToInt32(cmd.ExecuteScalar());
+
+
+                if (user_id != 0)
+                {
+                    this.Close();
+                    sales.Show();
+                }
+                else
+                {
+                    MessageBox.Show("Invalid username and password");
+                }
+
+
+            }
+            catch
+            {
+                MessageBox.Show("Error login");
+            }
+            finally
+            {
+                con.Close();
+            }
+        }*/
+
+        public bool IsDarkTheme { get; set; }
+        private readonly PaletteHelper paletteHelper = new PaletteHelper();
+        private void toggleTheme(object sender, RoutedEventArgs e)
+        {
+            ITheme theme = paletteHelper.GetTheme();
+            if (IsDarkTheme = theme.GetBaseTheme() == BaseTheme.Dark)
+            {
+                IsDarkTheme = false;
+                theme.SetBaseTheme(Theme.Light);
+            }
+            else
+            {
+                IsDarkTheme = true;
+                theme.SetBaseTheme(Theme.Dark);
+            }
+            paletteHelper.SetTheme(theme);
+        }
+
+        private void exitApp(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+
+        }
+        protected override void OnMouseLeftButtonDown(MouseButtonEventArgs e)
+        {
+            base.OnMouseLeftButtonDown(e);
+            DragMove();
+        }
+    }
+}
